0c6f3a208eaec20a9a964d825aee436eb873a332
Pawel Mroczkowski <tadeeek@gmail.com>
2021-05-14 00:28:55 +0200

    JwtSecret move to app properties

--- src/main/java/com/tadeeek/budgettracker/demo/util/JwtUtil.java
+++ src/main/java/com/tadeeek/budgettracker/demo/util/JwtUtil.java
@@ -16,7 +16,8 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String SECRET_KEY = "mysecret";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
