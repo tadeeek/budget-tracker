835e4e8289a2068c57c0a963ded836f91e1cbfd1
Pawel Mroczkowski <tadeeek@gmail.com>
2021-05-18 22:15:31 +0200

    validate user and get jwt

--- budget-tracker-app/src/components/Home.js
+++ budget-tracker-app/src/components/Home.js
@@ -1,8 +1,74 @@
 import React, { Component } from "react";
 
 class Home extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
+
+  initialState = {
+    username: "",
+    password: "",
+    errorMessage: "",
+  };
+
+  credentialsChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    console.log(this.state);
+  };
+
+  credentialsReset = () => {
+    this.setState(() => this.initialState);
+  };
+
+  validateUser = (event) => {
+    event.preventDefault();
+    this.authenticateUser(this.state.username, this.state.password);
+    // setTimeout(() => {
+    //   if ("jest zalogowany") {
+    //     costam
+    //   } else {
+    //     this.credentialsReset();
+    //     this.setState({ errorMessage: "Invalid username or password" });
+    //   }
+    // }, 600);
+  };
+
+  async authenticateUser(username, password) {
+    const credentials = {
+      username: username,
+      password: password,
+    };
+    console.log(JSON.stringify(credentials));
+    await fetch("/authenticate", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        console.log(data);
+        let token = data.token;
+        if (!response.ok) {
+          const error = data.message;
+          console.log("Response is not OK....");
+          return Promise.reject(error);
+        }
+        localStorage.setItem("jwtToken", token);
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error.toString() });
+        console.error("There was an error!", error);
+      });
+  }
+
   render() {
+    const { username, password, errorMessage } = this.state;
     return (
       <div className="home-bg min-vh-100">
         <div className="container">
@@ -14,16 +80,19 @@ class Home extends Component {
             <div className="home-signin text-center">
               <form>
                 <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-                <label for="inputUser" class="visually-hidden">
+                <label for="inputUserName" class="visually-hidden">
                   User
                 </label>
                 <input
                   type="text"
-                  id="inputUser"
+                  id="inputUserName"
                   class="form-control"
                   placeholder="User"
                   required=""
                   autofocus=""
+                  value={username}
+                  name="username"
+                  onChange={this.credentialsChange}
                 />
                 <label for="inputPassword" class="visually-hidden">
                   Password
@@ -34,13 +103,21 @@ class Home extends Component {
                   class="form-control"
                   placeholder="Password"
                   required=""
+                  value={password}
+                  type="password"
+                  name="password"
+                  onChange={this.credentialsChange}
                 />
                 <div class="checkbox mb-3">
                   <label>
                     <input type="checkbox" value="remember-me" /> Remember me
                   </label>
                 </div>
-                <button class="w-100 btn btn-lg btn-primary" type="submit">
+                <button
+                  class="w-100 btn btn-lg btn-primary"
+                  type="submit"
+                  onClick={this.validateUser}
+                >
                   Sign in
                 </button>
                 <p class="mt-5 mb-3 text-muted">©2021</p>
